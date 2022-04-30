306f39ae777c3ae1d8394c84f13185201b1ee692
gustavobetarelo <flavio.pprado4@gmail.com>
2022-04-30 16:55:11 -0300

    eslint

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,22 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { ClientsModule, Transport } from '@nestjs/microservices';
-import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { OrderModule } from './order/order.module';
+import { ProductModule } from './product/product.module';
 
 @Module({
-  imports: [
-    ClientsModule.register([
-      {
-        name: 'ORDER_MS',
-        transport: Transport.TCP,
-        options: {
-          host: '127.0.0.1',
-          port: 8888,
-        },
-      },
-    ]),
-  ],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [AuthModule, OrderModule, ProductModule],
 })
 export class AppModule {}
